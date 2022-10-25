ac64ba79ba3ce26d10b1fc4d940c8d2aecbaa432
Ivan <izuravel524@gmail.com>
2022-10-25 21:01:13 +0300

    create level_widget

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -8,7 +8,8 @@ abstract class AppColors {
   static const Color gradientEnd = Color(0xFFfc8ca1);
   static const Color blcokedButton = Color.fromRGBO(121, 121, 121, 0.3);
   static const Color gradientStartActive = Color(0xFF015fc6);
-  static const Color gradientEndActive = Color(0xFF007aff);
+  static const Color gradientEndActive = Color.fromRGBO(0, 122, 255, 0.8);
   static const Color gradientStartNonActive = Color(0xFF818080);
-  static const Color gradientEndNonActive = Color(0xFF999999);
+  static const Color gradientEndNonActive = Color.fromRGBO(153, 153, 153, 0.8);
+    static const Color progressIndicatorBg = Color(0xFFd8d8d8);
 }
