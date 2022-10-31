0c095c24165257fd696e9d4af78d3041509e76e7
Ivan <izuravel524@gmail.com>
2022-10-31 15:33:26 +0200

    fix navigation

--- lib/src/ui/profile/account/components/profile_text_field.dart
+++ lib/src/ui/profile/account/components/profile_text_field.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:polec/theme/app_colors.dart';
 
 class ProfileTextField extends StatelessWidget {
-  const ProfileTextField({super.key, required this.placeholder});
- final String placeholder;
+  const ProfileTextField({
+    super.key,
+    required this.placeholder,
+  });
+
+  final String placeholder;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoTextField(
-              decoration: BoxDecoration(
-                color: AppColors.bgAccountPage,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              placeholder: placeholder,
-            );
+      decoration: BoxDecoration(
+        color: AppColors.bgAccountPage,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      placeholder: placeholder,
+    );
   }
 }
--- lib/src/ui/profile/account/edit_profile_widget.dart
+++ lib/src/ui/profile/account/edit_profile_widget.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:polec/resources/colors.dart';
 import 'package:polec/src/common/models/user_preferences.dart';
 import 'package:polec/src/ui/details/components/recomended_button.dart';
+import 'package:polec/src/ui/profile/account/account_widget.dart';
 import 'package:polec/src/ui/profile/account/components/profile_text_field.dart';
+import 'package:polec/src/ui/profile/account/edit_components/edit_basic_information.dart';
 import 'package:polec/src/ui/profile/account/edit_components/gender_nav_bar.dart';
+import 'package:polec/src/ui/profile/components/nav_bar/navigation_bar.dart';
 import 'package:polec/theme/app_colors.dart';
 
-class EditProfileWidget extends StatelessWidget {
+class EditProfileWidget extends StatefulWidget {
   const EditProfileWidget({super.key});
 
+  @override
+  State<EditProfileWidget> createState() => _EditProfileWidgetState();
+}
+
+class _EditProfileWidgetState extends State<EditProfileWidget> {
   @override
   Widget build(BuildContext context) {
     final user = UserPreferences.myUser;
@@ -38,225 +46,7 @@ class EditProfileWidget extends StatelessWidget {
               style: textStyle,
             ),
             const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                'e-mail',
-                style: titleTextStyle,
-              ),
-            ),
-            const ProfileTextField(
-              placeholder: 'Email',
-            ),
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Name/names',
-                          style: titleTextStyle,
-                        ),
-                      ),
-                      const ProfileTextField(
-                        placeholder: 'Name/names',
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Surname',
-                          style: titleTextStyle,
-                        ),
-                      ),
-                      const ProfileTextField(
-                        placeholder: 'Surname',
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                'PESEL',
-                style: titleTextStyle,
-              ),
-            ),
-            const ProfileTextField(
-              placeholder: 'XXXXXXXXXXX',
-            ),
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 5),
-                      child: Text(
-                        'Phone',
-                        style: titleTextStyle,
-                      ),
-                    ),
-                    Container(
-                      width: 60,
-                      child: const ProfileTextField(
-                        placeholder: 'Phone',
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      ProfileTextField(
-                        placeholder: '666 666 666',
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Street and number',
-                          style: titleTextStyle,
-                        ),
-                      ),
-                      const ProfileTextField(
-                        placeholder: 'Street and number',
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Sity',
-                          style: titleTextStyle,
-                        ),
-                      ),
-                      const ProfileTextField(
-                        placeholder: 'Sity',
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Post code',
-                          style: titleTextStyle,
-                        ),
-                      ),
-                      const ProfileTextField(
-                        placeholder: 'XX-XXX',
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Container(),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Gender',
-                          style: titleTextStyle,
-                        ),
-                      ),
-                      const GenderNavBar(),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Container(),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Date of birth',
-                          style: titleTextStyle,
-                        ),
-                      ),
-                      const ProfileTextField(
-                        placeholder: '21 february 1993',
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Container(),
-                ),
-              ],
-            ),
+            const EditBasicInformation(),
             //////////////////////////////////////////////////////////////
             const SizedBox(height: 20),
             Text(
@@ -296,12 +86,20 @@ class EditProfileWidget extends StatelessWidget {
               ),
               imageColor: Colors.white,
               textColor: Colors.white,
-              onPressed: () => Navigator.pop(context),
+              onPressed: () {
+                setState(() {
+                  changeEditWidget();
+                });
+              },
             ),
-           const SizedBox(height: 10),
+            const SizedBox(height: 10),
           ],
         ),
       ),
     );
+  }
+
+  void changeEditWidget() {
+    detailWidget.update(Detail.account, (value) => const AccountWidget());
   }
 }
