705f111d65ce063614c80004bf3c6088e42f01e5
Ivan <izuravel524@gmail.com>
2022-11-14 12:03:53 +0200

    create account_bloc

--- lib/src/ui/profile/components/profile_app_bar.dart
+++ lib/src/ui/profile/components/profile_app_bar.dart
@@ -3,15 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:polec/resources/colors.dart';
 import 'package:polec/src/common/models/user_preferences.dart';
-import 'package:polec/src/feature/details/widget/details_screen.dart';
-import 'package:polec/src/ui/details/bloc/details_bloc.dart';
-import 'package:polec/src/ui/profile/account/account_widget.dart';
 import 'package:polec/src/ui/profile/account/bloc/account_bloc.dart';
 import 'package:polec/src/ui/profile/account/edit_profile_widget.dart';
 import 'package:polec/src/ui/profile/components/nav_bar/navigation_bar.dart';
-import 'package:polec/src/ui/profile/level/level_widget.dart';
-import 'package:polec/src/ui/profile/statistics/statistics_widget.dart';
-import 'package:polec/theme/app_colors.dart';
 
 class ProfileAppBar extends StatefulWidget {
   const ProfileAppBar({super.key});
@@ -24,9 +18,15 @@ class _ProfileAppBarState extends State<ProfileAppBar> {
   final user = UserPreferences.myUser;
 
   @override
+  void initState() {
+    context.read<AccountBloc>().add(const LoadAccount());
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final accountModel = context.read<AccountBloc>().state.accountModel;
-    //  if (accountModel == null) return const SizedBox.shrink();
+    if (accountModel == null) return const SizedBox.shrink();
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16, top: 50),
       child: Row(
@@ -36,7 +36,10 @@ class _ProfileAppBarState extends State<ProfileAppBar> {
             children: [
               CircleAvatar(
                 radius: 25,
-                backgroundColor: Colors.grey.shade400,
+                // backgroundColor: Colors.grey.shade400,
+                backgroundImage: AssetImage(
+                  accountModel.avatar.toString(),
+                ),
               ),
               const SizedBox(width: 10),
               Column(
@@ -44,7 +47,7 @@ class _ProfileAppBarState extends State<ProfileAppBar> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    '${accountModel?.firstName} ${user.surname}',
+                    '${accountModel.firstName} ${accountModel.lastName}',
                     style: TextStyle(
                       color: AppColor.titleColor,
                       fontSize: 16,
