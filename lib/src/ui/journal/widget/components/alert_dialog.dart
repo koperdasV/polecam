daf2a62d6bcff4cdf6effbe727913d2691cc2287
Vasyl Koperdas <koperdasb@gmail.com>
2022-11-17 19:59:55 +0200

    fix removed pagination in the card

--- lib/src/ui/journal/widget/components/alert_dialog.dart
+++ lib/src/ui/journal/widget/components/alert_dialog.dart
@@ -2,17 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:polec/src/ui/details/components/show_dialog.dart';
 import 'package:polec/src/ui/home/widget/components/in_active_button.dart';
 import 'package:polec/src/ui/journal/model/journal/journal_model.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class AlertDialogWidget extends StatelessWidget {
   const AlertDialogWidget({
     Key? key,
-    required PageController controller,
     required this.journalModel,
-  })  : _controller = controller,
-        super(key: key);
+  }) : super(key: key);
 
-  final PageController _controller;
   final JournalModel journalModel;
 
   @override
@@ -46,20 +42,17 @@ class AlertDialogWidget extends StatelessWidget {
               textAlign: TextAlign.center,
               '${journalModel.target.name} recommends\n With your first purchase'
               '\nYou will get a 30% discount',
-              style: const TextStyle(),
+              style: const TextStyle(
+                height: 1.5,
+              ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: SmoothPageIndicator(
-              controller: _controller, // PageController
-              count: 3,
-              onDotClicked: (index) {},
+          const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: InActiveButton(
+              text: 'Save the Card',
+              fontSize: 22,
             ),
-          ),
-          const InActiveButton(
-            text: 'Save the Card',
-            fontSize: 22,
           ),
         ],
       ),
