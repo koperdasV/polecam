570a4ff77ea8a880fa559e5d8f93cddc6c7dc35c
Ivan <izuravel524@gmail.com>
2022-12-13 13:05:35 +0200

    fix statistics

--- lib/src/ui/profile/statistics/payout_page/components/list_widget.dart
+++ lib/src/ui/profile/statistics/payout_page/components/list_widget.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:polec/src/ui/journal/bloc/journals_bloc.dart';
+import 'package:polec/src/ui/journal/model/journal/journal_model.dart';
 
 class ListWidget extends StatelessWidget {
   const ListWidget({
-    Key? key, required this.name, required this.money, required this.color,
+    Key? key,
+    required this.name,
+    required this.money,
+    required this.color,
   }) : super(key: key);
 
   final String name;
@@ -11,56 +17,63 @@ class ListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      slivers: [
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 10,
-            (context, index) => Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 19,
-                vertical: 15,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+    return BlocBuilder<JournalsBloc, JournalsState>(
+      builder: (context, state) {
+        return CustomScrollView(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          slivers: [
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                childCount: state.journals.length - 10,
+                (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 19,
+                    vertical: 15,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 15),
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundColor: color,
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 15),
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundColor: color,
+                              backgroundImage: NetworkImage(
+                                state.journals[index].source.avatar,
+                              ),
+                            ),
+                          ),
+                          Text(
+                             state.journals[index].source.name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        name,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 16,
-                        ),
+                      Row(
+                        children: [
+                          Text(
+                            money,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        money,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
