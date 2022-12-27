357aa4cf89c79586c0c1b40f9431c8250acfdaf5
Vasyl Koperdas <koperdasb@gmail.com>
2022-12-27 10:25:53 +0200

    fix current-location

--- lib/src/ui/map/map_widget.dart
+++ lib/src/ui/map/map_widget.dart
@@ -9,37 +9,53 @@ import 'package:polec/src/ui/home/blocs/blocs.dart';
 import 'package:polec/src/ui/map/markerx.dart';
 
 class MapWidget extends StatefulWidget {
-  const MapWidget({super.key});
+  MapWidget({super.key});
+
+  late Position position;
+  bool isLoad = false;
 
   @override
   State<MapWidget> createState() => _MapWidgetState();
 }
 
 class _MapWidgetState extends State<MapWidget> {
-  final _defaulLat = 49.362532;
-  final _defaulLng = 23.4914867;
   double? lat;
   double? long;
-  
-  Future<Position> _getCurrentLocation() async {
-    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disable');
-    }
 
-    var permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permission are denied');
-      }
-    }
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-        'Location permissions are permanently denied, we cannot request',
-      );
-    }
-    return Geolocator.getCurrentPosition();
+  Future<void> getPosition() async {
+    widget.position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    setState(() {
+      widget.isLoad = true;
+    });
+  }
+
+  // Future<Position> _getCurrentLocation() async {
+  //   final serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  //   if (!serviceEnabled) {
+  //     return Future.error('Location services are disable');
+  //   }
+
+  //   var permission = await Geolocator.checkPermission();
+  //   if (permission == LocationPermission.denied) {
+  //     permission = await Geolocator.requestPermission();
+  //     if (permission == LocationPermission.denied) {
+  //       return Future.error('Location permission are denied');
+  //     }
+  //   }
+  //   if (permission == LocationPermission.deniedForever) {
+  //     return Future.error(
+  //       'Location permissions are permanently denied, we cannot request',
+  //     );
+  //   }
+  //   return Geolocator.getCurrentPosition();
+  // }
+
+  @override
+  void initState() {
+    getPosition();
+    super.initState();
   }
 
   @override
@@ -60,42 +76,45 @@ class _MapWidgetState extends State<MapWidget> {
         if (state.status == RecommendedStateStatus.success) {
           return Stack(
             children: [
-              FlutterMap(
-                options: MapOptions(
-                  center: LatLng(
-                    _defaulLat,
-                    _defaulLng,
-                  ),
-                  zoom: 14,
-                ),
-                children: [
-                  TileLayer(
-                    urlTemplate:
-                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    userAgentPackageName: 'com.example.app',
-                  ),
-                  MarkerLayer(
-                    rotate: true,
-                    markers: state.recommended
-                        .map((e) => MarkerX(recommendedModel: e))
-                        .toList(growable: false),
-                  ),
-                  Positioned(
-                    bottom: 10,
-                    right: 10,
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        _getCurrentLocation().then((value) {
-                          lat = value.latitude;
-                          long = value.longitude;
-                        });
-                        print('Latitude: $lat, Longitude: $long');
-                      },
-                      child: const Icon(CupertinoIcons.location),
+              if (widget.isLoad)
+                FlutterMap(
+                  options: MapOptions(
+                    center: LatLng(
+                      widget.position.latitude,
+                      widget.position.longitude,
                     ),
+                    zoom: 14,
                   ),
-                ],
-              ),
+                  children: [
+                    TileLayer(
+                      urlTemplate:
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      userAgentPackageName: 'com.example.app',
+                    ),
+                    MarkerLayer(
+                      rotate: true,
+                      markers: state.recommended
+                          .map((e) => MarkerX(recommendedModel: e))
+                          .toList(growable: false),
+                    ),
+                    // Positioned(
+                    //   bottom: 10,
+                    //   right: 10,
+                    //   child: FloatingActionButton(
+                    //     onPressed: () {
+                    //       // _getCurrentLocation().then((value) {
+                    //       //   lat = value.latitude;
+                    //       //   long = value.longitude;
+                    //       // });
+                    //       print('Latitude: $lat, Longitude: $long');
+                    //     },
+                    //     child: const Icon(CupertinoIcons.location),
+                    //   ),
+                    // ),
+                  ],
+                )
+              else
+                const Center(child: CircularProgressIndicator()),
             ],
           );
         } else {
